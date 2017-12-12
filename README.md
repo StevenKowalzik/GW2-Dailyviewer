1d1235a5733e620c60c1a908d119a8d732f634d6
Steven Kowalzik <stevenk@fb3.uni-bremen.de>
2017-12-12 14:35:55 +0100

    Extracted JS and refactor to use fetch and promises

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gw2dailyviewer
-This shows the daily achievements for guildwars 2
+
+This shows the daily achievements for GuildWars 2
+
+# ToDo
+ * Better Styling (Layout, Icons)
+ * WikiLinks?
+ * Show Dailies for the Day after
+ * Non-ES6 Fallback
---
+++ app.js
@@ -0,0 +1,14 @@
+
+const baseUrl = 'https://api.guildwars2.com/v2/achievements';
+
+fetch(`${baseUrl}/daily`)
+  .then(res => res.json())
+  .then(data => Object.keys(data).map((type) => {
+    const el = document.querySelector(`.${type}`);
+    Promise.all(data[type].map(am => fetch(`${baseUrl}?ids=${am.id}`)
+      .then(res => res.json())))
+      .then(achie => achie.map((x) => {
+        el.innerHTML += `${x[0].name} <br />`;
+      }));
+  }))
+  .catch(err => console.log(err));
