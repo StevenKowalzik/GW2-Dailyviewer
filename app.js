fa7d2a34e20e47be5d27e24aa18ddb2d3704f65d
StevenKowalzik <stvnkwlzk@googlemail.com>
2018-03-18 23:57:31 +0100

    today and tomorrow

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const baseUrl = 'https://api.guildwars2.com/v2/achievements';
 fetch(`${baseUrl}/daily`)
   .then(res => res.json())
   .then(data => Object.keys(data).map((type) => {
-    const el = document.querySelector(`.${type}`);
+    const el = document.querySelector(`.today .${type}`);
     Promise.all(data[type].map(am => fetch(`${baseUrl}?ids=${am.id}`)
       .then(res => res.json())))
       .then(achie => achie.map((x) => {
@@ -12,3 +12,15 @@ fetch(`${baseUrl}/daily`)
       }));
   }))
   .catch(err => console.log(err));
+
+  fetch(`${baseUrl}/daily/tomorrow`)
+    .then(res => res.json())
+    .then(data => Object.keys(data).map((type) => {
+      const el = document.querySelector(`.tomorrow .${type}`);
+      Promise.all(data[type].map(am => fetch(`${baseUrl}?ids=${am.id}`)
+        .then(res => res.json())))
+        .then(achie => achie.map((x) => {
+          el.innerHTML += `${x[0].name} <br />`;
+        }));
+    }))
+    .catch(err => console.log(err));
